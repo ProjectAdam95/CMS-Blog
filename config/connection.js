c9029d159024f366c1bfb28adf44c7019ab73b21
Adam Todorovic <a.tod95@hotmail.com>
2024-09-08 06:26:54 +1000

    changes to connection.js for render

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,  // This is important for cloud-based PostgreSQL connections
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+      }
+    );
 
 module.exports = sequelize;
+
