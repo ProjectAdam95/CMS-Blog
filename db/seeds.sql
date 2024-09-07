009db2b4799bc5ae71c3d2f133834a5f9d724150
Adam Todorovic <a.tod95@hotmail.com>
2024-09-08 07:07:38 +1000

    changes to db names table/comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,14 @@
--- Inserting into the post table with current timestamp
-INSERT INTO "post" (title, content, user_id, created_at, updated_at)
+-- Inserting into the posts table with current timestamp
+INSERT INTO "posts" (title, content, user_id, created_at)
 VALUES
-('First Blog Post by Adam', 'This is a post by Adam Todorovic!', 1, NOW(), NOW()),
-('First Blog Post by Jane', 'This is a post by Jane Doe!', 2, NOW(), NOW());
+('First Blog Post by Adam', 'This is a post by Adam Todorovic!', 1, NOW()),
+('First Blog Post by Jane', 'This is a post by Jane Doe!', 2, NOW());
 
--- Inserting into the comment table with current timestamp
-INSERT INTO "comment" (comment_text, post_id, user_id, created_at, updated_at)
+-- Inserting into the comments table with current timestamp
+INSERT INTO "comments" (comment_text, post_id, user_id, created_at)
 VALUES
-('Great post, Adam!', 1, 2, NOW(), NOW()),  -- post_id = 1
-('Nice one, Jane!', 2, 1, NOW(), NOW()),    -- post_id = 2
-('Well written, John!', 1, 1, NOW(), NOW()); -- post_id = 1
+('Great post, Adam!', 1, 2, NOW()),  -- post_id = 1
+('Nice one, Jane!', 2, 1, NOW()),    -- post_id = 2
+('Well written, John!', 1, 1, NOW()); -- post_id = 1
+
 
